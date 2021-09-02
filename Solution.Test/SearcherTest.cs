af1cdd142f63fd6bd988f8eb2f29575f3e5a88ac
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2021-09-02 19:53:14 +0300

    Task completed

---
+++ Solution.Test/SearcherTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Solution.Test
+{
+    [TestFixture]
+    public class SearcherTest
+    {
+        private static IEnumerable<TestCaseData> testData
+        {
+            get
+            {
+                //------------------------------------
+                yield return new TestCaseData(new int[] { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, 5, 4);
+                yield return new TestCaseData(new int[] { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, 15, 14);
+                yield return new TestCaseData(new int[] { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, 0, -1);
+                yield return new TestCaseData(new int[] { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, -5, -1);
+                yield return new TestCaseData(new int[] { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, 324, -1);
+                //------------------------------------
+                yield return new TestCaseData(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 5, 4);
+                yield return new TestCaseData(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 15, 14);
+                yield return new TestCaseData(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 0, -1);
+                yield return new TestCaseData(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, -5, -1);
+                yield return new TestCaseData(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 324, -1);
+                //------------------------------------
+                yield return new TestCaseData(new float[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 5, 4);
+                yield return new TestCaseData(new float[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 15, 14);
+                yield return new TestCaseData(new float[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 0, -1);
+                yield return new TestCaseData(new float[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, -5, -1);
+                yield return new TestCaseData(new float[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 100, -1);
+                //------------------------------------
+                yield return new TestCaseData(new string[] { "asd", "asddsa", "as"}, "as", 0);
+                yield return new TestCaseData(new string[] { "asd", "asddsa", "as" }, "asdasd", -1);
+                yield return new TestCaseData(new string[] { "as", "dasd", "agsfdgf", "qweasdghfhghkhjl", "qweadsfgh", "qwaesgdhfjhk", "qwaesgdhfjhks" }, "qwaesgdhfjhks", 4);
+            }
+        }
+
+        [TestCaseSource(nameof(testData))]
+        public void TestAnytingType<T>(T[] data, T element, int expectedIndex)
+        {
+            Array.Sort(data);
+            Assert.AreEqual(expectedIndex, Searcher.Search(data, element));
+        }
+
+        [Test]
+        public void TrownArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Searcher.Search(null, 5));
+        }
+    }
+}
---
+++ Solution/Searcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    public static class Searcher
+    {
+        public static int Search<T>(T[] data, T elementToSearch)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            int left = 0;
+            int right = data.Length - 1;
+            var comparer = Comparer<T>.Default;
+
+            while (left <= right)
+            {
+                var middle = (left + right) / 2;
+                var number = comparer.Compare(data[middle], elementToSearch);
+
+                if (number == 0)
+                {
+                    return middle;
+                }
+                else if (number > 0)
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
